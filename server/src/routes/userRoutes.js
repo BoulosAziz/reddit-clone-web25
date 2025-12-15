a5cd55ae691b7ba2880be5876992d155980213aa
Boulos Aziz <boulos.wagdy@gmail.com>
2025-12-15 15:15:07 +0200

    Add user search by username

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,9 +1,12 @@
 //server/src/routes/userRoutes.js
 import express from "express";
-import { getMe, updateMe } from "../controllers/userController.js";
+import { getMe, updateMe, searchUsers } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+
+// Search users
+router.get("/search", protect, searchUsers);
 
 // GET /api/users/me     → Get profile
 router.get("/me", protect, getMe);
