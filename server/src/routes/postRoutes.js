a80adf16509fa251377f5101fd39d13d98688f2e
Carol Rimoun <academiccarol@gmail.com>
2025-12-15 18:28:48 +0200

    Added delete post and delete comment

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,18 +1,30 @@
-//server/src/routes/postRoutes.js
-import express from "express"; 
-import { protect } from "../middleware/authMiddleware.js"; 
-import { createPost, getPostsByCommunity, votePost } 
-from "../controllers/postController.js";
+import express from "express";
+import { protect } from "../middleware/authMiddleware.js";
+import {
+  createPost,
+  getPostsByCommunity,
+  votePost,
+  deletePost
+} from "../controllers/postController.js";
+import { checkPostOwner } from "../middleware/postMiddleware.js";
+
 const router = express.Router();
-
-
 
 // Create post
 router.post("/", protect, createPost);
 
 // Get posts by community
 router.get("/community/:communityId", getPostsByCommunity);
+
 // Upvote / downvote
 router.post("/:postId/vote", protect, votePost);
+
+// Delete post
+router.delete(
+  "/:postId",
+  protect,
+  checkPostOwner,
+  deletePost
+);
 
 export default router;
