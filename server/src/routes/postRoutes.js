0328bc3dc05692431974ba78c3d1e44c8654790f
Farida Tawfik <ferytawfik90@gmail.com>
2025-12-15 13:35:34 +0200

    finished communities backend

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -1,3 +1,4 @@
+//postRoutes.js
 const express = require('express');
 const router = express.Router();
 const { authMiddleware } = require('../middleware/authMiddleware');
--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,3 +1,4 @@
+//userRoutes.js
 import express from "express";
 import { getMe, updateMe } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
