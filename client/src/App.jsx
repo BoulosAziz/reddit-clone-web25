4f6ba72f06e7da3802dbc1257c1da40a4a3abd6e
Martin <tinomagued@gmail.com>
2025-12-17 18:03:07 +0200

    Fix include files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MainLayout from "./components/layout /MainLayout.jsx";
+import MainLayout from "./components/layout/MainLayout.jsx";
 import Home from "./pages/Home.jsx";
 import Post from "./pages/Post.jsx";
 import CreatePost from "./pages/CreatePost.jsx";
